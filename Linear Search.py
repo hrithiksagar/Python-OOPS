00b7e67b504293f4be3776f3d2f62b611bbbd419
Hrithik <hrithiksagar36@gmail.com>
2023-07-22 19:59:56 +0530

    linear search

---
+++ Linear Search.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9]
+n = 9
+
+pos = -1
+def search(list,n):
+    i=0
+    while i<len(list):
+        if list[i]==n:
+            globals()['pos'] = i
+            return True
+        i = i+1
+    return False
+            
+if search(list,n):
+    print("Element found at", + pos)
+else:
+    print("Not Found!")
+    
